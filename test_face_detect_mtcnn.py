42c63daa60c4d9670bed454cdf0bd979f6c9030d
benfeard <benfeard@gmail.com>
2020-10-19 21:01:30 -0400

    Create test_face_detect_mtcnn.py
    New code to test face detection

---
+++ test_face_detect_mtcnn.py
@@ -0,0 +1,22 @@
+'''
+by Benfeard Williams
+on Mon Oct 19 2020
+'''
+
+import pytest
+from face_detect_mtcnn.py import *
+
+
+# test to see how many faces are detectd in test1.jpg
+# inputs = 
+# outputs = 
+
+def test_results():
+
+    #get user input
+
+    #run function
+   
+
+    #test function
+    assert 
